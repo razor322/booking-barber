161c7458dacd4edf602104139da6b23e85ebd9af
gybran nauval <nauvalgybran@gmail.com>
2025-10-15 23:58:17 +0700

    refactor(constants): add constants for new image assets

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -3,6 +3,11 @@
 // --- IMAGES ---
 // Impor semua gambar yang Anda butuhkan di sini
 import heroImage from "@/assets/images/barber-hero.jpg";
+import guy1 from "@/assets/images/guy1.webp";
+import guy2 from "@/assets/images/guy2.webp";
+import guy3 from "@/assets/images/guy3.webp";
+import user from "@/assets/images/user.png";
+import logo from "@/assets/images/logo.png";
 
 // import logo from "@/assets/images/logo.svg";
 
@@ -14,7 +19,11 @@ import heroImage from "@/assets/images/barber-hero.jpg";
 export const AppAssets = {
   images: {
     hero: heroImage,
-    // logo: logo,
+    guy1: guy1,
+    guy2: guy2,
+    guy3: guy3,
+    user: user,
+    logo: logo,
   },
   icons: {
     // a: iconA,
