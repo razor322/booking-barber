d782a86b0f9f40d2f52a7ed944d1f134f2949831
gybran nauval <nauvalgybran@gmail.com>
2025-10-15 23:59:21 +0700

    feat(routing): define routes for authentication pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Mainlayout from "./components/layouts/Mainlayot";
+import NotFound from "./pages/NotFound";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
 
 export default function Home() {
   return (
     <Routes>
       <Route element={<Mainlayout />}>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/booking" element={<BookingPage />} /> */}
+        <Route path="/auth/login" element={<LoginPage />} />
         {/* <Route path="/about" element={<AboutPage />} /> */}
       </Route>
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/auth/register" element={<RegisterPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
